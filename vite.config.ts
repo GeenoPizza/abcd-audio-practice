eef0c6a0b455bf2da50492f5d87aefa30ee18138
GeenoPizza <matteoferrandomusic@gmail.com>
2025-12-30 23:28:11 +0100

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
+      manifest: {
+        name: 'ABCD Audio Practice',
+        short_name: 'ABCD Audio',
+        description: 'Metodo ABCD per musicisti',
+        theme_color: '#5dda9d',
+        background_color: '#0b0d0e',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
